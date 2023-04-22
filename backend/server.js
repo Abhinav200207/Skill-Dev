f0042efd4af83868569d0719a55d5a6679a9fcca
Himanshu Singh <himanishu93@gmail.com>
2023-04-23 01:47:36 +0530

    Feature:Login and Signup Page with Backend Integration

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = require("./app.js");
 const mongoose = require('mongoose'); // Importing Mongoose
 
 
+
 const connectDatabase = () => {
     return mongoose.connect("mongodb+srv://mongo:ABHKPV2020@cluster0.ng3en0j.mongodb.net/?retryWrites=true&w=majority").then((data)=>{
         console.log(`Connected to database data`) // log this statement if connection is successful
